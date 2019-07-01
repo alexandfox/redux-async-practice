27c4497e0a9bccfe8b87b53f84e79fe7b7ec73b5
alex fox <to.alexfox@gmail.com>
2019-07-01 16:33:22 -0500

    setup thunk middleware in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { selectSubreddit, fetchPosts } from './actions/actions'
+import rootReducer from './reducers/reducers'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  )
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPosts('reactjs')).then(() => console.log(store.getState()))
+
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
